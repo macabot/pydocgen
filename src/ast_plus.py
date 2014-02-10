fe4b39e93e694b9e7e5e3ed3b5af577c05b40110
Michael Cabot <michael.a.cabot@gmail.com>
2014-02-10 16:29:41 +0100

    annotate an AST so that important nodes can be extracted to form a sentence.

--- src/ast_plus.py
+++ src/ast_plus.py
@@ -21,13 +21,14 @@ LABEL_MAP = {'Attribute': 'attr',
              'FunctionDef': 'name',
              'Name': 'id',
              'Num': 'n'}
-IGNORE_MAP = set(['arguments',
+IGNORE_MAP = set([#'arguments',
                   'BinOp',
                   'BoolOp',
-                  'Call',
+                  #'Call',
+                  #'Compare',
                   'Expr',
                   'Load',
-                  'Module',
+                  #'Module',
                   'Param',
                   'Store',
                   'Str', # ignore or map to 's', i.e. content of the string?
@@ -35,67 +36,107 @@ IGNORE_MAP = set(['arguments',
 
 class ASTPlus(object):
 
-    def __init__(self, ast_tree):
+    def __init__(self, ast_tree, split = False):
+        # TODO make trees uniform, e.g. order arguments of Equals alphabetically
         add_parents(ast_tree)
-        add_labels(ast_tree)
+        add_labels(ast_tree, split)
         add_factors(ast_tree)
+        add_ignore_labels(ast_tree)
         self.tree = ast_tree
+
+    def parallel_functions(self, filters = None):
+        """For each function in the tree return a tuple with the words in its
+        docstring and the important nodes in it's tree. Ignore functions without
+        a docstring."""
+        return parallel_functions(self.tree, filters)
+
+    def draw(self, show_factors = None, show_ignore = False):
+        """Convert an ast tree to a nltk tree"""
+        draw(self.tree, show_factors, show_ignore)
+
+
+def parallel_functions(tree, filters = None, parallel = None):
+    """For each function in the tree return a tuple with the words in its
+    docstring and the important nodes in it's tree. Ignore functions without
+    a docstring."""
+    if parallel == None:
+        parallel = []
+
+    if tree.__class__.__name__ == 'FunctionDef':
+        docstring = ast.get_docstring(tree)
+        if docstring != None:
+            sentence = ' '.join(tree_to_words(tree))
+            parallel.append((clean_doc(docstring, filters), sentence, tree))
+
+    for child in ast.iter_child_nodes(tree):
+        parallel_functions(child, filters, parallel)
+
+    return parallel
+    
+def clean_doc(docstring, filters = None):
+    """Filter unimportant parts of the docstring and make all words white space
+    separated."""
+    if filters != None:
+        for filter in filters:
+            docstring = filter(docstring)
+    docstring = re.sub(r'\s+', ' ', docstring.strip())
+    return docstring
+
+def tree_to_words(tree, words = None):
+    """Extract the important tree labels from the tree."""
+    if words == None:
+        words = []
         
-    def draw(self, show_factors = None):
-        draw(self.tree, show_factors)
+    if not tree.ignore:
+        words.append(tree.label)
+    for child in ast.iter_child_nodes(tree):
+        tree_to_words(child, words)
         
+    return words
+    
+def add_ignore_labels(tree):
+    class_name = tree.__class__.__name__
+    tree.ignore = class_name in IGNORE_MAP
+    for child in ast.iter_child_nodes(tree):
+        add_ignore_labels(child)
 
 def set_parents(tree, parent = None):
+    """Add to each node a pointer to its parent."""
     tree.parent = parent
     for child in ast.iter_child_nodes(tree):
         add_parents(child, tree)
 
-def tokenize(tree, tokens = None):
-    if tokens == None:
-        tokens = []
-
+def get_name_map(tree, name_map):
+    """If the class name is in the name map, return an attribute of the tree."""
     class_name = tree.__class__.__name__
-    token = get_name_map(tree, TOKENIZE_MAP)
-    if token != '':
-        tokens.append(token)
-
-    for child in ast.iter_child_nodes(tree):
-        tokenize(child, tokens)
-
-    return tokens
-
-def get_name_map(tree, map):
-    class_name = tree.__class__.__name__
-    if class_name in map:
-        return str(getattr(tree, map[class_name]))
+    if class_name in name_map:
+        return str(getattr(tree, name_map[class_name]))
     return class_name
-
-def tokenize_tree(tree, self_class = None):
-    parent_name = get_name_map(tree, LABEL_MAP)
-    if tree.__class__.__name__ == 'ClassDef':
-        self_class = parent_name
-    child_names = []
-    for child in ast.iter_child_nodes(tree):
-        token = tokenize_tree(child, self_class)
-        if token != '':
-            if token == 'self':
-                token += '_' + self_class
-            child_names.append(token)
-    if len(child_names) == 0:
-        return parent_name
-    return '(%s %s)' % (parent_name, ' '.join(child_names))
 
 def split_camel_case(name):
     # see http://stackoverflow.com/a/1176023/854488
     #return [name.lower()]
     return [name]
 
-def add_labels(tree):
+def add_labels(tree, split = False):
+    """Add labels to all nodes in the tree."""
     tree.label = get_name_map(tree, LABEL_MAP)
+    if split:
+        tree.label = ' '.join(split_label(tree.label))
     for child in ast.iter_child_nodes(tree):
-        add_labels(child)
+        add_labels(child, split)
+
+def split_label(label):
+    """Split a label into a list of words that it contains.
+    TODO split camelCase
+    TODO split getters/setters, e.g. 'getsource' >> ['get', 'source']"""
+    if '_' in label:
+        return label.strip('_').split('_')
+    return [label]
 
 def add_factors(tree, class_def_name = None):
+    """Add factors to all nodes in the tree.
+    TODO explain all factors"""
     class_name = tree.__class__.__name__
     if class_name == 'ClassDef':
         class_def_name = class_name
@@ -105,7 +146,7 @@ def add_factors(tree, class_def_name = None):
     context = getattr(tree, 'ctx', None)
     tree.factors['context'] = str(getattr(context, 'label', context))
     tree.factors['class'] = str(class_def_name)
-    
+
     for child in ast.iter_child_nodes(tree):
         add_factors(child, class_def_name)
 
@@ -121,16 +162,17 @@ def add_parents(tree, parent = None):
     for child in ast.iter_child_nodes(tree):
         add_parents(child, tree)
 
-def draw(tree, show_factors = None):
+def draw(tree, show_factors = None, show_ignore = False):
     """Convert the tree to a nltk tree and draw it."""
-    from nltk import Tree
-    ast_to_nltk_tree(tree, show_factors).draw()
+    ast_to_nltk_tree(tree, show_factors, show_ignore).draw()
 
-def ast_to_nltk_tree(ast_tree, show_factors = None):
+def ast_to_nltk_tree(ast_tree, show_factors = None, show_ignore = False):
     """Convert an ast tree to a nltk tree"""
     from nltk import Tree
     parent_name = ast_tree.label
     if show_factors:
         parent_name += '|'
         parent_name += '|'.join(ast_tree.factors[key] for key in show_factors)
-    return Tree(parent_name, [ast_to_nltk_tree(child, show_factors) for child in ast.iter_child_nodes(ast_tree)])
+    if show_ignore and ast_tree.ignore:
+        parent_name = 'I_' + parent_name
+    return Tree(parent_name, [ast_to_nltk_tree(child, show_factors, show_ignore) for child in ast.iter_child_nodes(ast_tree)])
