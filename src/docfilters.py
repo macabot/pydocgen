351c415b61141d39e8ac273135316b695cfb71ab
Michael Cabot <michael.a.cabot@gmail.com>
2014-02-12 22:57:49 +0100

    catch exception when parsing docstring for doctests

--- src/docfilters.py
+++ src/docfilters.py
@@ -1,10 +1,17 @@
 from doctest import DocTestParser, Example
+import logging
+
+logging.basicConfig(filename='./log.txt', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 def remove_doctests(docstring):
     """Parse a docstring and remove doctest examples."""
     parser = DocTestParser()
-    examples_and_strings = parser.parse(docstring)
-    return ''.join(s for s in examples_and_strings if not isinstance(s, Example))
+    try:
+        examples_and_strings = parser.parse(docstring)
+        return ''.join(s for s in examples_and_strings if not isinstance(s, Example))
+    except ValueError as e:
+        logging.exception('\nValueError when removing doctest from:\n---\n%s\n---\n%s\n' % (docstring, e))
+        return docstring
     
 def remove_parameter_descriptions(docstring):
     """Remove parameter descriptions that start with a colon. 
