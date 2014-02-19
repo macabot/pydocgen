90faf84176f1f96a0cf34a66e3e5b8b3252622d7
Michael Cabot <michael.a.cabot@gmail.com>
2014-02-19 21:57:41 +0100

    create a main: read input folder and output folder from command line arguments

--- src/trainer.py
+++ src/trainer.py
@@ -1,10 +1,17 @@
+"""
+By Michael Cabot
+
+Create a parallel corpus of a python project.
+"""
+
 import zipfile
 import os
 import ast
 import logging
 import sys
+import argparse
 
-from ast_plus import ASTPlus
+import ast_plus
 import unparse
 import utils
 
@@ -14,7 +21,7 @@ logging.basicConfig(filename='./log.txt', level=logging.DEBUG, format='%(asctime
 
 
 def create_parallel_corpus(zip_path, out_path, use_factors = True, max_count = float('inf')):
-    """For all python files in the given zip file, create a parallel corpus of 
+    """For all python files in the given zip file, create a parallel corpus of
     docstring words and source code words."""
     if not os.path.isfile(zip_path) or os.path.splitext(zip_path)[1] != '.zip':
         raise ValueError('invalid zip path: %s' % zip_path)
@@ -23,11 +30,12 @@ def create_parallel_corpus(zip_path, out_path, use_factors = True, max_count = f
     doc_path = out_path + '.doc'
     info_path = out_path + '.info'
     count = 0
+    progress_chars = 60
     with open(sc_path, 'w') as sc_out, open(doc_path, 'w') as doc_out, open(info_path, 'w') as info_out:
         info_out.write('<zip_path>%s</zip_path>\n' % zip_path)
         with zipfile.ZipFile(zip_path, 'r') as zip_file:
             for py_path in iter_py_in_zip_file(zip_file):
-                sys.stdout.write('\r%s' % utils.uniform_string(py_path))
+                sys.stdout.write('\r%s' % utils.uniform_string(py_path, progress_chars))
                 sys.stdout.flush()
                 with zip_file.open(py_path, 'r') as py_file:
                     try:
@@ -41,7 +49,7 @@ def create_parallel_corpus(zip_path, out_path, use_factors = True, max_count = f
                         print 'py_path: %s' % py_path
                         raise
 
-                    tree = ASTPlus(ast_tree)
+                    tree = ast_plus.ASTPlus(ast_tree)
 
                     for docstring, source_code_words, tree in tree.parallel_functions(use_factors):
                         if count >= max_count:
@@ -57,7 +65,8 @@ def create_parallel_corpus(zip_path, out_path, use_factors = True, max_count = f
                             print 'py_path: %s' % py_path
                             print 'lineno: %s' % tree.lineno
                             raise
-    print
+    sys.stdout.write('\r%s\r' % utils.uniform_string('', progress_chars))
+    sys.stdout.flush()
 
 def write_info(info_out, tree, count, py_path):
     """Write to info_out the following information:
@@ -142,12 +151,33 @@ def test_create_parallel_corpus():
     print 'done'
 
 def main():
-    """main"""
-    pass
+    """Read command line arguments for creating a parallel corpus."""
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-i', '--input_path', required=True,
+        help='Path to folder containing python projects as zip-files')
+    arg_parser.add_argument('-o', '--output_path', required=True,
+        help='Path of folder for output.')
+
+    args = arg_parser.parse_args()
+
+    input_path = args.input_path
+    if not os.path.isdir(input_path):
+        raise ValueError('Invalid input path: %s' % input_path)
+    output_path = args.output_path
+    if not os.path.isdir(output_path):
+        raise ValueError('Invalid output folder: %s' % output_path)
+
+    for zip_path in iter_files_with_extension(input_path, '.zip'):
+        print zip_path
+        in_basename = os.path.basename(zip_path)
+        in_root, _ext = os.path.splitext(in_basename)
+        out_root = os.path.join(output_path, in_root)
+        create_parallel_corpus(zip_path, out_root)
+    print 'done'
 
 
 if __name__ == '__main__':
-    #main()
+    main()
     #test_zipf()
     #test_extract_rules_from_zip()
-    test_create_parallel_corpus()
+    #test_create_parallel_corpus()
