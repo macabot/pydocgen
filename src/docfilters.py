ff8a9c5b502cb4ab8f1f0f57b532fa704d986369
Michael Cabot <michael.a.cabot@gmail.com>
2014-02-20 20:50:59 +0100

    add filter to keep only first segment of a docstring

--- src/docfilters.py
+++ src/docfilters.py
@@ -1,5 +1,13 @@
+"""
+By Michael Cabot
+
+Filters for removing unimportant parts from docstrings.
+"""
+
+
 from doctest import DocTestParser, Example
 import logging
+import re
 
 logging.basicConfig(filename='./log.txt', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
@@ -12,6 +20,18 @@ def remove_doctests(docstring):
     except ValueError as e:
         logging.exception('\nValueError when removing doctest from:\n---\n%s\n---\n%s\n' % (docstring, e))
         return docstring
+
+def test_remove_doctests():
+    """test remove_doctests"""
+    docstring = """
+    belong a doctest
+    >>> a = 3
+    >>> a
+    3
+
+    end of doctest
+    """
+    print remove_doctests(docstring)
 
 def remove_parameter_descriptions(docstring):
     """Remove parameter descriptions that start with a colon.
@@ -29,8 +49,37 @@ def remove_wx_wrappers(docstring):
     """Remove docstrings that are wrappers. Examples:
     IsOk(self) -> bool
     Focus(self, long index)
-    TODO test
     """
     if re.search(r'\S+\(.*\)', docstring.strip()):
         return ''
     return docstring
+
+def test_remove_wx_wrappers():
+    """test remove_wx_wrappers"""
+    assert remove_wx_wrappers("""IsOk(self) -> bool""")==''
+    assert remove_wx_wrappers("""Focus(self, long index)""")==''
+    assert remove_wx_wrappers("""Focus(self, long index""")!=''
+
+def keep_first_description(docstring):
+    """Remove everything after an empty line"""
+    return re.split(r'\n\s*\n', docstring)[0]
+    
+def test_keep_first_description():
+    """test keep_first_description"""
+    docstring = """
+    first description
+    over multiple
+    lines
+    
+    second
+    
+    third
+    """
+    print keep_first_description(docstring)
+    print keep_first_description("""foo""")
+
+
+if __name__ == '__main__':
+    #test_remove_doctests()
+    #test_remove_wx_wrappers()
+    test_keep_first_description()
