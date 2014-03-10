c60990a75bba84dca645be749a1ff0b26d0666a3
Michael Cabot <michael.a.cabot@gmail.com>
2014-03-10 22:19:56 +0100

    possibility to add existing weight_scores. add missing value top_translations. write weight_scores to file.

--- src/pattern_search.py
+++ src/pattern_search.py
@@ -8,6 +8,7 @@ import re
 import math
 import argparse
 import os
+import sys
 
 import decoder
 import mp_worker
@@ -72,7 +73,7 @@ def get_bleu_score(bleu_path, reference, hypothesis):
     match = re.search(r'(?<=BLEU = )\d+(\.\d+)?', bleu_out)
     assert match != None, 'unknown bleu output: %s' % bleu_out
     return float(match.group(0))
-    
+
 def test_get_bleu_score():
     """test get_bleu_score"""
     bleu_path = '/home/bart/apps/smt_tools/decoders/mosesdecoder/scripts/generic/multi-bleu.perl'
@@ -80,14 +81,18 @@ def test_get_bleu_score():
     hypothesis = '/home/michael/pydocgen/data/decode/blankdefault/tunesearch_NOfactors.doc_STACK100TOP10BEAM1CORE30'
     print get_bleu_score(bleu_path, reference, hypothesis)
 
-def optimize(weights, get_score, step_size, min_diff, max_iterations):
+def optimize(weights, get_score, step_size, min_diff, max_iterations, 
+             weight_scores = None):
     """Optimize the score with pattern search"""
+    if weight_scores == None:   
+        weight_scores = {}
     diff = float('inf')
     iteration = 0
     best_score = get_score(weights)
     best_weights = weights
-    weight_scores = {tuple(weights):best_score} # map weights to score
-    while diff > min_diff and iteration < max_iterations:
+    weight_scores[tuple(weights)] = best_score # map weights to score
+    while diff >= min_diff and iteration <= max_iterations:
+        sys.stdout.write('\ri:{i}<={max}'.format(i=iteration, max=max_iterations))
         previous_best_weights = best_weights[:]
         current_weights = best_weights[:]
         for i in xrange(len(weights)):
@@ -113,20 +118,22 @@ def optimize(weights, get_score, step_size, min_diff, max_iterations):
                     diff = score - best_score
                     best_score = score
                     best_weights = subtract_weights
-        
+
         if previous_best_weights == best_weights:
             step_size /= 2.0
 
         iteration += 1
-    return (best_weights, best_score), weight_scores
     
+    sys.stdout.write('\ri:{i}<={max}\n'.format(i=iteration, max=max_iterations))
+    return (best_weights, best_score), weight_scores
+
 def test_optimize():
     """test optimize"""
     get_score = lambda weights: -(weights[0]**2 + weights[1]**2)
     step_size = 1.0
     min_diff = 0.001
-    max_iterations = 30
-    (best_weights, best_score), _weight_scores = optimize([3.1, 4.1], get_score,
+    max_iterations = 3000
+    (best_weights, best_score), _weight_scores = optimize([300.1, 400.1], get_score,
                                                          step_size,
                                                          min_diff,
                                                          max_iterations)
@@ -136,6 +143,24 @@ def test_optimize():
     # for k, v in weight_scores.iteritems():
         # print k, v
 
+def weight_scores_to_file(weight_scores, path):
+    """write weight_scores to file"""
+    with open(path, 'w') as out:
+        for weights, score in weight_scores.iteritems():
+            out.write('{weights} ||| {score}\n'.format(
+                weights=' '.join(str(weight) for weight in weights),
+                score=score))
+
+def read_weight_scores(path):
+    """Read weight_scores from file"""
+    weight_scores = {}
+    with open(path, 'r') as in_file:
+        for line in in_file:
+            weights, score = line.strip().split(' ||| ')
+            weights = [float(w) for w in weights.split()]
+            score = float(score)
+            weight_scores[weights] = score
+    return weight_scores
 
 def main():
     """Read command line arguments."""
@@ -195,8 +220,16 @@ def main():
         help="Path to multi-bleu.perl")
     arg_parser.add_argument('-ref', '--reference', required=True,
         help="Path to reference translations")
+    arg_parser.add_argument('-ws', '--weight_scores', default=None,
+        help="Path to existing weight scores")
 
     args = arg_parser.parse_args()
+    
+    weight_scores_path = args.weight_scores
+    if weight_scores_path != None:
+        weight_scores = read_weight_scores(weight_scores_path)
+    else:
+        weight_scores = None
 
     input_file = args.input_file
     output_file = args.output_file
@@ -216,19 +249,33 @@ def main():
     feature_weights = args.feature_weights
     nbest = args.nbest
     empty_default = args.empty_default
+    top_translations = args.top_translations
     bleu_path = args.bleu_path
     reference = args.reference
 
     d_args = DecodeArguments(input_file, output_file, language_model,
                  source_language_model, args.translation_model, max_lines,
                  beam_size, args.processes, max_phrase_length, stack_limit,
-                 stupid_backoff, n_size, nbest, empty_default, bleu_path,
-                 reference)
+                 stupid_backoff, n_size, nbest, empty_default, top_translations,
+                 bleu_path, reference)
 
     get_score = d_args.get_bleu_score
 
-    optimize(feature_weights, get_score, args.step_size, args.min_score_diff,
-             args.max_iterations)
+    print 'initial weights:'
+    print feature_weights
+    (best_weights, best_score), weight_scores = optimize(feature_weights,
+                                                         get_score,
+                                                         args.step_size,
+                                                         args.min_score_diff,
+                                                         args.max_iterations,
+                                                         weight_scores)
+    print 'final weights:'
+    print best_weights
+    print 'final score: %f' % best_score
+
+    weight_scores_name = output_file + '.weightscores'
+    weight_scores_to_file(weight_scores, weight_scores_name)
+    print 'weight_scores written to: %s' % weight_scores_name
 
 
 if __name__ == '__main__':
