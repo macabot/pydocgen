5904807f1ade030fe852e23e48a0304d7776b8b4
Michael Cabot <michael.a.cabot@gmail.com>
2014-03-10 22:21:32 +0100

    with empty translations and blanks for unknown words

--- src/tunesearch.bat
+++ src/tunesearch.bat
@@ -2,7 +2,7 @@ PHRASE_LENGTH="7"
 STACK="100"
 BACKOFF="0.4"
 N_SIZE="3"
-TOP="10 50 100"
+TOP="10"
 BEAM="1 10 100"
 CORE="30"
 
@@ -12,18 +12,19 @@ do
     do
         for beam in `echo $BEAM`
         do
-            out_path=/home/michael/pydocgen/data/decode/tunesearch/tunesearch_NOfactors.doc_STACK${stack}TOP${top}BEAM${beam}CORE$CORE
+            out_path=/home/michael/pydocgen/data/decode/empty_and_blank/tunesearch_NOfactors.doc_STACK${stack}TOP${top}BEAM${beam}CORE$CORE
             python /home/michael/pydocgen/src/decoder.py \
             -i /home/michael/pydocgen/data/tune/tune_clean_docstring-filtered_sourcecode-NOcontext-NOfactors.tok.sc \
             -o $out_path \
             -lm /home/michael/pydocgen/data/lm/lmpy_train_clean_docstring-filtered.tok.doc \
             -lms /home/michael/pydocgen/data/lm/lmpy_train_clean_sourcecode-NOcontext-NOfactors.tok.sc \
-            -tm /home/michael/pydocgen/data/phrases/multicore/phrases_NOfactors_30proc_alllines_7phraselength_all_info.txt \
+            -tm /home/michael/pydocgen/data/phrases/multicore/empty_phrases_NOfactors_alllines_7phraselength_all_info.txt \
             -mpl $PHRASE_LENGTH \
             -sl $stack \
             -bs $beam \
             -sb $BACKOFF \
             -tt $top \
+            -ed \
             -pr $CORE | tee ${out_path}_stdout
 
             perl /home/bart/apps/smt_tools/decoders/mosesdecoder/scripts/generic/multi-bleu.perl '/home/michael/pydocgen/data/tune/tune_clean_docstring-filtered_sourcecode-NOcontext-NOfactors.tok.doc'  < $out_path |tee ${out_path}_BLEU
